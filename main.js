71b4304e856d098782be0f84e78a8ab2fc8f6a57
dunphy1996 <dunphy1996@gmail.com>
2024-09-17 14:29:01 +0000

    More work

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var messages = []
+var messages = localStorage.getItem('messages') ? JSON.parse(localStorage.getItem('messages')) : [];
 
 function updateMessages() {
     const main = document.querySelector('#chat-window');
     main.innerHTML = '';
  
-    for (let i = messages.length - 1; i >= 0; i--) {
+    for (let i = 0; i < messages.length; i++) {
         const message = messages[i];
         const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
+        messageElement.classList = 'message primary padding margin right-round top-round'
         messageElement.textContent = message;
         main.appendChild(messageElement);
     }
@@ -19,11 +19,12 @@ function sendMessage() {
     messages.push(message);
     input.value = '';
     updateMessages();
+    localStorage.setItem('messages', JSON.stringify(messages));
   }
 
 document.querySelector('button').addEventListener('click', sendMessage);
 document.querySelector('input').addEventListener('keypress', (event) => {
-if (event.key === 'Enter') {
+if (event.key === 'Enter' && !event.shiftKey) {
     sendMessage();
 }
 });
